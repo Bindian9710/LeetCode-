07d8c86ad7091e4adb4970c12bd3f4c4202b721e
Wolf <nnlcccc@outlook.com>
2020-04-21 10:56:03 +0800

    删除重复文件

--- LeetCode66-加一.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def plusOne(self, digits):
-        dig = int(''.join(str(i) for i in digits))+1
-        return [int(i) for i in list(str(dig))]
--- LeetCode724-寻找数组中心索引.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def pivotIndex(self, nums):
-        numAll = sum(nums)
-        for i in range(len(nums)):
-            if sum(nums[:i])*2+nums[i] == numAll:
-                return i
-        return -1
-
